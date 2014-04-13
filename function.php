b03bc81d6b4bff4f56c48679ae18b658d4a5cd58
haierspi <haierspi@qq.com>
2014-04-13 13:11:00 +0800

    删除没用文件,提交 附件以及头像更新

--- function.php
+++ function.php
@@ -54,11 +54,55 @@ function showmnextpage($message,$nexturl=false){
 	exit;
 }
 
+function get_avatar_path($uid){
+
+	$uid = abs(intval($uid));
+	$uid = sprintf("%09d", $uid);
+	$dir1 = substr($uid, 0, 3);
+	$dir2 = substr($uid, 3, 2);
+	$dir3 = substr($uid, 5, 2);
+
+	$path = $dir1.'/'.$dir2.'/'.$dir3.'/'.substr($uid, -2).'_avatar_$size$.jpg';
+	nmkdir($path);
+	return  $path;
+}
+
+function mv_avatar($uid,$olduid) {
+	$retrue = TRUE;
+	$path  = get_avatar_path($uid);
+	$oldpath  = get_avatar_path($olduid);
+	foreach (array('big', 'middle', 'small') as  $value) {
+		$retrue  = $retrue && rename(AVATARPATH_OLD.str_replace('$size$', $value, $oldpath),AVATARPATH.str_replace('$size$', $value, $path));
+	}
+	return $retrue;
+}
 
 
+function mv_attach($aid,$tableid) {
+	$attach= DB::fetch_first("SELECT * FROM ".DB::table('forum_attachment_'.$tableid)." WHERE `aid`='$aid'" );
 
+	nmkdir(ATTACHPATH.$attach['attachment']);
+	if (file_exists(ATTACHPATH_OLD.$attach['attachment'].'.thumb.jpg')) {
+		@rename(ATTACHPATH_OLD.$attach['attachment'].'.thumb.jpg',ATTACHPATH.$attach['attachment'].'.thumb.jpg');
+	}
+	return @rename(ATTACHPATH_OLD.$attach['attachment'],ATTACHPATH.$attach['attachment']);
+}
 
+function nmkdir($path, $mode = 0777){
 
+	$path = str_replace ( '\\', '/',  dirname($path) );
+	$dir = '';
+	$dirarray = explode ( '/', $path );
+	foreach ( $dirarray as $str ) {
+		$dir .= $str . '/';
+		if (! file_exists ( $dir )) {
+			mkdir ( $dir, $mode );
+		}
+	}
+		
+	@touch($dir.'/index.html'); @chmod($dir.'/index.html', 0777);
+
+}
 
 
 ?>
